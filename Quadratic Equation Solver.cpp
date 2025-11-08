79908b09519ddd94ea6770062e89fdf7824ee020
devgleden <umayamgleden@gmail.com>
2025-11-08 12:30:31 +0900

    Update Quadratic Equation Solver.cpp

--- Quadratic Equation Solver.cpp
+++ Quadratic Equation Solver.cpp
@@ -10,7 +10,7 @@ double constant(char quadraticConstant)
 	
 	while (!validInput)
 	{
-		std::cout << quadraticConstant << ": ";
+		std::cout <<"Enter coefficient " <<quadraticConstant << ": ";
 		std::cin >> a;
 
 		if (std::cin.fail())
@@ -26,49 +26,47 @@ double constant(char quadraticConstant)
 	}
 		
 	return a;
+
 }
-
 double root1(double a, double b, double c)
-
 {
 	return (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
 }
 
 double root2(double a, double b, double c)
-
 {
 	 return (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
 }
 
 void quadraticRoots(double a, double b, double c)
 {
-	if (a == 0) 
-	{
-		std::cout << "Error: 'A' cannot be zero in a quadratic equation!\n";
-		return ;
-	}
-
-	double solvedRoot1{ root1(a, b, c) };
-	double solvedRoot2{ root2(a, b, c) };
 	double discriminant{ (b * b) - (4 * a * c) };
 
-	if (discriminant > 0) 
+	if (a == 0) 
 	{
+		std::cout << "Error: coefficient 'A' cannot be zero in a quadratic equation!\n";
+		return ;
+	}
+	
+	else if (discriminant > 0) 
+	{
+		double solvedRoot1{ root1(a, b, c) };
+		double solvedRoot2{ root2(a, b, c) };
 		std::cout << " The roots are: " << solvedRoot1 << " and " << solvedRoot2 << '\n';
 	}
 		
 	else if (discriminant < 0)
 	{
-		double realPart{ -b / 2 * a };
-		double imaginaryPart{ sqrt(-discriminant) / 2 * a };
+		double realPart{ -b / (2 * a) };
+		double imaginaryPart{ sqrt(-discriminant) / (2 * a) };
 
 		std::complex<double> complexRoot1{ realPart, imaginaryPart };
 		std::complex<double> complexRoot2{ realPart, -imaginaryPart };
-		std::cout << "Roots are imaginary (real part, imaginary part): " << complexRoot1 << " and " << complexRoot2;
+		std::cout << "\nRoots are complex numbers (real part, imaginary part): " << complexRoot1 << " and " << complexRoot2 <<'\n';
 	}
 	else
 	{
-		std::cout << "The root is: " << -b / 2 * a << '\n';
+		std::cout << "The root is: " << -b / (2 * a) << '\n';
 	}
 }
 
